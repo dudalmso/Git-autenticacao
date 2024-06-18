c8b7b2d64698bbd857b8d36ee638790b1a7e3352
Duda Lima <160052447+dudalmso@users.noreply.github.com>
2024-06-18 19:16:56 +0000

    alterei

---
+++ arqv/concat_dados.py
@@ -0,0 +1,7 @@
+
+info1 = input('Digite primeira informação:')
+info2 = input('Digite segunda informação:')
+
+info_concatenado = info1 + " " + info2
+
+print('As informações ocncatenadas são: ', info_concatenado)
